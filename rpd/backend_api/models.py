9de99cae34d006c7197d703e5b3fa16d7db0129c
HoverGo <kotikarbuz@gmail.com>
2023-10-30 00:52:56 +0500

    Yra, poluchilos

--- rpd/backend_api/models.py
+++ rpd/backend_api/models.py
@@ -3,5 +3,6 @@ from django.db import models
 
 class contactForm(models.Model):
     name = models.CharField("Имя", max_length=100)
-    number = models.IntegerField("Номер телефона")
-    text = models.TextField("Комментарий ")
+    phone = models.IntegerField("Номер телефона")
+    comments = models.TextField("Комментарий ")
+    privacyPolicy = models.BooleanField("Приватность", default=False)
